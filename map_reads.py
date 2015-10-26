63ff21f459a19592dbbadd9b25847d459200954c
calizarr <calizarr@gmail.com>
2015-10-26 15:23:06 -0500

    added and fixed map_reads

---
+++ map_reads.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+from __future__ import print_function
+import argparse
+import subprocess
+import os
+import sys
+
+
+def options():
+    parser = argparse.ArgumentParser(description="Map ATAC-seq reads with bowtie")
+    parser.add_argument("nThreads", help="Number of threads to use for bowtie")
+    parser.add_argument("input_file", help="Read file to be mapped with bowtie")
+    parser.add_argument("out_file_dir", help="Directory for output file (directory for the sam files)")
+    parser.add_argument("basepre", help="Base prefix for output file (Everything before .sam)")
+    parser.add_argument("-i", "--indices", help="Directory of bowtie indices (default: Brachy)")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = options()
+    # Getting the absolute path to file if relative path given.
+    args.input_file = os.path.abspath(args.input_file)
+    # Getting the directory name
+    dirname = os.path.dirname(args.input_file)
+    # Getting just the filename
+    output_file = os.path.join(dirname, args.out_file_dir, args.basepre)
+    print(output_file)
+    if not args.indices:
+        indices = "/shares/tmockler_share/clizarraga/Brachypodium_distachyon/Phytozome/v3.1/assembly/indices/Bdistachyon_314_v3.0.hardmasked"
+    else:
+        indices = args.indices
+    # Map reads to reference
+    cmd = "bowtie --chunkmbs 256 -p {0} -S -m 1 -X 2000 -t {1} {2} | samtools view - -bS | samtools sort - -o {3} -O bam -T {2}.pre" \
+          .format(args.nThreads, indices, args.input_file, output_file)
+    print("Running cmd: ")
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+    # Only mapped reads.
+    cmd = "samtools view -b -F {0} > {0}.sorted.bam".format(output_file)
+    print("Running cmd: ")
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+
+if __name__ == "__main__":
+    main()
